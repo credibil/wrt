1f6788e1add757209f4273efea0d13b527826af2
andrew_weston <andrew.weston@propellerhead.co.nz>
2025-09-11 16:20:01 +1200

    fix cargo-deny issues

---
+++ RELEASES.md
@@ -0,0 +1,15 @@
+## 0.2.0
+
+Unreleased.
+
+### Added
+
+### Changed
+
+---
+
+Release notes for previous releases can be found on the respective release 
+branches of the repository.
+
+<!-- ARCHIVE_START -->
+* [0.1.x](https://github.com/credibil/core/blob/release-0.1.0/RELEASES.md)
--- src/wg_http.rs
+++ src/wg_http.rs
@@ -1,14 +1,10 @@
 use anyhow::Result;
-use dotenv::dotenv;
 use runtime::{Cli, Command, Parser, Runtime};
 use wasi_http::Http;
 use wasi_otel::Otel;
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    if cfg!(debug_assertions) {
-        dotenv().ok();
-    }
     match Cli::parse().command {
         Command::Run { wasm } => Runtime::new(wasm).register(Otel).register(Http).await,
         #[cfg(feature = "compile")]
