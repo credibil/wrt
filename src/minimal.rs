c7cd50488707e3540f25194807c32e700e3a39f6
quynhduongphl <135781267+quynhduongphl@users.noreply.github.com>
2025-10-10 07:59:45 +1300

    Start telemetry early (#58)
    * Separate runtime init sequence so tracing/logging can start before other steps.
    
    * Remove unnecessary method
    
    * Make warning message more descriptive
    
    * Simplify builder

--- src/minimal.rs
+++ src/minimal.rs
@@ -1,6 +1,6 @@
 use anyhow::{Result, anyhow};
 use res_mongodb::MongoDb;
-use runtime::{AddResource, Cli, Command, Parser, ResourceBuilder, Runtime};
+use runtime::{AddResource, Cli, Command, Parser, ResourceBuilder, RuntimeBuilder};
 use wasi_blobstore_mdb::Blobstore;
 use wasi_http::Http;
 use wasi_otel::Otel;
@@ -10,6 +10,11 @@ async fn main() -> Result<()> {
     let Command::Run { wasm } = Cli::parse().command else {
         return Err(anyhow!("only run command is supported"));
     };
+    let builder = RuntimeBuilder::new(wasm, true);
+    tracing::info!("Tracing initialised, logging available");
+
     let mongodb = MongoDb::new().await?;
-    Runtime::new(wasm).register(Otel).register(Http).register(Blobstore.resource(mongodb)?).await
+    let runtime =
+        builder.register(Otel).register(Http).register(Blobstore.resource(mongodb)?).build();
+    runtime.await
 }
