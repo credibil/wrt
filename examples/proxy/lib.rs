b5afd9bedd725f2576b37e61e8fb7fe6fb07c50e
Andrew Goldie <goldie@worksend.com>
2025-10-16 12:37:09 +1300

    first pass of proxy example

--- examples/proxy/lib.rs
+++ examples/proxy/lib.rs
@@ -1,11 +1,10 @@
 #![cfg(target_arch = "wasm32")]
 
-use axum::routing::{options, post};
+use anyhow::Context;
+use axum::routing::{get, post};
 use axum::{Json, Router};
 use http::Method;
-use opentelemetry::trace::{TraceContextExt, Tracer};
-use opentelemetry::{KeyValue, global};
-use sdk_http::Result;
+use sdk_http::{Client, Decode, Result};
 use serde_json::{Value, json};
 use tower_http::cors::{Any, CorsLayer};
 use tracing::Level;
@@ -16,63 +15,56 @@ struct HttpGuest;
 
 impl Guest for HttpGuest {
     #[sdk_otel::instrument(name = "http_guest_handle",level = Level::DEBUG)]
-    fn handle(request: IncomingRequest, response: ResponseOutparam) {
-        // tracing metrics
-        tracing::info!(monotonic_counter.tracing_counter = 1, key1 = "value 1");
-        tracing::info!(gauge.tracing_gauge = 1);
+    fn handle(request: IncomingRequest, response_out: ResponseOutparam) {
+        let router = Router::new()
+            .route("/", get(get_handler))
+            .layer(
+                CorsLayer::new()
+                    .allow_methods([Method::GET, Method::POST, Method::OPTIONS])
+                    .allow_headers(Any)
+                    .allow_origin(Any),
+            )
+            .route("/", post(post_handler));
 
-        // otel metrics
-        let meter = global::meter("my_meter");
-        let counter = meter.u64_counter("otel_counter").build();
-        counter.add(1, &[KeyValue::new("key1", "value 1")]);
-
-        // otel span
-        let tracer = global::tracer("basic");
-        tracer.in_span("main-operation", |cx| {
-            let span = cx.span();
-            span.set_attribute(KeyValue::new("my-attribute", "my-value"));
-            span.add_event("main span event", vec![KeyValue::new("foo", "1")]);
-
-            tracer.in_span("child-operation", |cx| {
-                cx.span().add_event("sub span event", vec![KeyValue::new("bar", "1")]);
-            });
-
-            // tracing span within otel span
-            tracing::info_span!("child info span").in_scope(|| {
-                tracing::info!("info event");
-            });
-        });
-
-        let out = tracing::info_span!("handler span").in_scope(|| {
-            tracing::info!("received request");
-            let router = Router::new()
-                .layer(
-                    CorsLayer::new()
-                        .allow_methods([Method::GET, Method::POST, Method::OPTIONS])
-                        .allow_headers(Any)
-                        .allow_origin(Any),
-                )
-                .route("/", post(handler))
-                .route("/", options(handle_options));
-            sdk_http::serve(router, request)
-        });
-
-        ResponseOutparam::set(response, out);
+        let out = sdk_http::serve(router, request);
+        ResponseOutparam::set(response_out, out);
     }
 }
 
-// A simple "Hello, World!" endpoint that returns the client's request.
-#[axum::debug_handler]
+// Forward request to external service and return the response
 #[sdk_otel::instrument]
-async fn handler(Json(body): Json<Value>) -> Result<Json<Value>> {
-    tracing::info!("handling request: {:?}", body);
+async fn get_handler() -> Result<Json<Value>> {
+    let body = Client::new()
+        .cache_bucket("credibil_bucket")
+        .get("https://jsonplaceholder.cypress.io/posts/1")
+        .send()?
+        .json::<Value>()
+        .context("issue sending request")?;
+
     Ok(Json(json!({
-        "message": "Hello, World!",
-        "request": body
+        "response": body
+    })))
+}
+
+// Forward request to external service and return the response
+#[sdk_otel::instrument]
+async fn post_handler(Json(body): Json<Value>) -> Result<Json<Value>> {
+    let body = Client::new()
+        .cache_bucket("credibil_bucket")
+        .post("https://jsonplaceholder.cypress.io/posts")
+        .bearer_auth("some token") // not required, but shown for example
+        .json(&body)
+        .send()?
+        .json::<Value>()
+        .context("issue sending request")?;
+
+    Ok(Json(json!({
+        "response": body
     })))
 }
 
 // Handle preflight OPTIONS requests for CORS.
+#[sdk_otel::instrument]
 async fn handle_options() -> Result<()> {
     Ok(())
 }
