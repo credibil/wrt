85fee48b226eb4b8a62c6da043d1dfeb2b450f1e
Andrew Weston <andrew.weston@propellerhead.co.nz>
2025-10-20 16:51:26 +1300

    rough build macro

--- src/standard.rs
+++ src/standard.rs
@@ -12,28 +12,35 @@ use wasi_vault::WasiVault;
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    let Command::Run { wasm } = Cli::parse().command else {
-        return Err(anyhow!("only run command is supported"));
-    };
-    let builder = RuntimeBuilder::new(wasm, true);
-    tracing::info!("Tracing initialised, logging available");
+    // let Command::Run { wasm } = Cli::parse().command else {
+    //     return Err(anyhow!("only run command is supported"));
+    // };
+    // let builder = RuntimeBuilder::new(wasm, true);
+    // tracing::info!("Tracing initialised, logging available");
 
-    let (mongodb, nats, az_vault) =
-        tokio::try_join!(MongoDb::new(), Nats::new(), AzKeyVault::new())?;
+    // let (mongodb, nats, az_vault) =
+    //     tokio::try_join!(MongoDb::new(), Nats::new(), AzKeyVault::new())?;
 
-    let messaging = WasiMessaging.resource(nats.clone()).await?;
-    let keyvalue = WasiKeyValue.resource(nats).await?;
-    let blobstore = WasiBlobstore.resource(mongodb).await?;
-    let vault = WasiVault.resource(az_vault).await?;
+    // let messaging = WasiMessaging.resource(nats.clone()).await?;
+    // let keyvalue = WasiKeyValue.resource(nats).await?;
+    // let blobstore = WasiBlobstore.resource(mongodb).await?;
+    // let vault = WasiVault.resource(az_vault).await?;
 
-    let runtime = builder
-        .register(WasiOtel)
-        .register(WasiHttp)
-        .register(blobstore)
-        .register(keyvalue)
-        .register(messaging)
-        .register(vault)
-        .build();
+    // let runtime = builder
+    //     .register(WasiOtel)
+    //     .register(WasiHttp)
+    //     .register(blobstore)
+    //     .register(keyvalue)
+    //     .register(messaging)
+    //     .register(vault)
+    //     .build();
 
-    runtime.await
+    // runtime.await;
+
+    build_macro::buildgen! ({
+        messaging: [Nats],
+        keyvalue: [Nats],
+        blobstore: [MongoDb],
+        vault: [AzKeyVault],
+    });
 }
