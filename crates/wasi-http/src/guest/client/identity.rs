32a6f772613e2fbdfca45c26557fdfa10963ad71
Andrew Weston <andrew.weston@propellerhead.co.nz>
2025-10-29 06:31:52 +1300

    remove unused

--- crates/wasi-http/src/guest/client/identity.rs
+++
@@ -1,6 +0,0 @@
-
-// let config = rustls::ClientConfig::builder()
-//         .with_safe_defaults()
-//         .with_root_certificates(root_store)
-//         .with_client_auth_cert(client_cert, client_key)
-//         .context("failed to attach client certificate to TLS config")?;
