6b92b5d3835776bcf09d9f9c7970dc59654a6f4a
andrei-biriukov-phl <andrei.biriukov@propellerhead.co.nz>
2025-10-29 11:43:44 +1300

    test and fix for cert usage in http

--- examples/proxy.rs
+++ examples/proxy.rs
@@ -1,8 +1,11 @@
 #![cfg(target_arch = "wasm32")]
 
+use std::println;
+
 use anyhow::Context;
 use axum::routing::{get, post};
 use axum::{Json, Router};
+use base64ct::{Base64, Encoding};
 use bytes::Bytes;
 use http::Method;
 use http::header::{CACHE_CONTROL, IF_NONE_MATCH};
@@ -31,29 +34,32 @@ async fn handle_cache() -> Result<Json<Value>> {
     let max_age = "max-age=300";
     let cache_key = "qf34lofge4rstep95tsiz9r75";
     // PEM-encoded private key and certificate
-    let auth_cert = "\
-        -----BEGIN CERTIFICATE-----
-        ...
-        -----BEGIN PRIVATE KEY-----
-        ...
-    ";
-
+    let auth_cert = "-----BEGIN CERTIFICATE-----
+...Your Certificate Here...
+-----END CERTIFICATE----- 
+-----BEGIN PRIVATE KEY-----
+...Your Private Key Here...
+-----END PRIVATE KEY-----";
+    println!("DEBUG Building request to cache service...");
+    let encoded_cert = Base64::encode_string(auth_cert.as_bytes());
     let request = http::Request::builder()
         .method(Method::GET)
-        .uri("https://jsonplaceholder.cypress.io/posts/1")
+        //.uri("https://jsonplaceholder.cypress.io/posts/1")
+        .uri("https://cert-traffic.tomtom.com/tsq/hdf/NZL-HDF-OPENLR/d1ab4fdc-9108-43d4-9480-7b6da08506d2/content.proto")
         .header(CACHE_CONTROL, max_age)
         .header(IF_NONE_MATCH, cache_key)
-        .header("Client-Cert", auth_cert)
+        .header("Client-Cert", &encoded_cert)
         .extension(CacheOptions {
             bucket_name: "example-bucket".to_string(),
             ttl_seconds: 300,
         })
         .body(Empty::<Bytes>::new())
         .expect("Failed to build request");
-
     let response = wasi_http::handle(request).await?;
     let body = response.into_body();
-    let body = serde_json::from_slice::<Value>(&body).context("issue parsing response body")?;
+    // let body = serde_json::from_slice::<Value>(&body).context("issue parsing response body")?;
+
+    let body_str = Base64::encode_string(&body);
 
     // let body = Client::new()
     //     .get("https://jsonplaceholder.cypress.io/posts/1")
@@ -64,7 +70,7 @@ async fn handle_cache() -> Result<Json<Value>> {
     //     .json::<Value>()?;
 
     Ok(Json(serde_json::json!({
-        "cached_response": body
+        "cached_response": body_str
     })))
 }
 
