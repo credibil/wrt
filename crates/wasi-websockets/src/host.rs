130c1b6ea4c17950240e4ba8b7e069b7347d5655
Quynh Duong <quynh.duong@propellerhead.co.nz>
2025-10-30 10:23:12 +1300

    Add send-peers method, fix fmt

--- crates/wasi-websockets/src/host.rs
+++ crates/wasi-websockets/src/host.rs
@@ -4,8 +4,6 @@
 #![allow(missing_docs)]
 mod generated {
     #![allow(clippy::trait_duplication_in_bounds)]
-    //pub use anyhow::Error;
-
     wasmtime::component::bindgen!({
         world: "websockets",
         path: "wit",
@@ -15,30 +13,28 @@ mod generated {
     });
 }
 
-use futures_channel::mpsc::{UnboundedSender, unbounded};
-use futures_util::{SinkExt, StreamExt, future, pin_mut, stream::TryStreamExt};
 use std::collections::HashMap;
 use std::env;
 use std::net::SocketAddr;
 use std::sync::{Arc, Mutex as StdMutex};
-use tokio::net::{TcpListener, TcpStream};
-use tokio::sync::{Mutex, OnceCell};
-use tokio_tungstenite::tungstenite::{Message, Utf8Bytes};
-use tokio_tungstenite::{MaybeTlsStream, WebSocketStream, accept_async, connect_async};
 
 use anyhow::Result;
 use futures::future::{BoxFuture, FutureExt};
+use futures_channel::mpsc::{UnboundedSender, unbounded};
+use futures_util::stream::TryStreamExt;
+use futures_util::{SinkExt, StreamExt, future, pin_mut};
 use runtime::RunState;
+use tokio::net::{TcpListener, TcpStream};
+use tokio::sync::{Mutex, OnceCell};
+use tokio_tungstenite::tungstenite::{Bytes, Message};
+use tokio_tungstenite::{MaybeTlsStream, WebSocketStream, accept_async, connect_async};
 use wasmtime::component::{HasData, InstancePre, Linker};
 use wasmtime_wasi::ResourceTable;
 
+use self::generated::wasi::websockets::handler;
+use self::generated::wasi::websockets::types::{Error, Peer};
+
 const DEF_HTTP_ADDR: &str = "0.0.0.0:80";
-
-use self::generated::wasi::websockets::{handler, types::Error};
-
-pub struct WebSocketMessage {
-    pub content: String,
-}
 
 /// Websockets service
 #[derive(Debug)]
@@ -73,25 +69,71 @@ impl Host<'_> {
 }
 
 impl handler::Host for Host<'_> {
-    async fn send(&mut self, message: String) -> Result<(), Error> {
+    async fn get_peers(&mut self) -> Result<Vec<Peer>, Error> {
+        let peer_map = PEER_MAP.get().ok_or_else(|| Error {
+            message: "Peer map not initialized".into(),
+        })?;
+        let peers = peer_map
+            .lock()
+            .unwrap()
+            .iter()
+            .map(|(key, peer)| Peer {
+                address: key.to_string(),
+                query: peer.query.clone(),
+            })
+            .collect();
+        Ok(peers)
+    }
+
+    async fn send_peers(&mut self, message: String, peers: Vec<String>) -> Result<(), Error> {
         tracing::info!("WebSocket write: {}", message);
+        let ws_client = service_client().await;
+        let msg = serde_json::to_string(&PublishMessage {
+            peers: peers.join(","),
+            content: message,
+        })
+        .unwrap();
+        let _ = ws_client.lock().await.send(Message::Text(msg.into())).await;
+        Ok(())
+    }
+
+    async fn send_all(&mut self, message: String) -> Result<(), Error> {
+        tracing::info!("WebSocket write: {}", message);
+        let ws_client = service_client().await;
+        let msg = serde_json::to_string(&PublishMessage {
+            peers: "all".into(),
+            content: message,
+        })
+        .unwrap();
+        let _ = ws_client.lock().await.send(Message::Text(msg.into())).await;
+        Ok(())
+    }
+
+    async fn health_check(&mut self) -> Result<String, Error> {
         let ws_client = SERVICE_CLIENT
             .get_or_init(|| async {
                 let addr = env::var("WEBSOCKETS_ADDR").unwrap_or_else(|_| DEF_HTTP_ADDR.into());
                 connect_service_client(format!("ws://{addr}").as_str()).await.unwrap()
             })
             .await;
-        let _ = ws_client.lock().await.send(Message::Text(Utf8Bytes::from(message))).await;
-        Ok(())
-    }
-
-    async fn health_check(&mut self) -> Result<String, Error> {
+        ws_client.lock().await.send(Message::Ping(Bytes::new())).await.map_err(|e| Error {
+            message: format!("Websocket service is unhealthy: {e}"),
+        })?;
         Ok("websockets service is healthy".into())
     }
 }
 
-type Tx = UnboundedSender<Message>;
-type PeerMap = Arc<StdMutex<HashMap<SocketAddr, Tx>>>;
+#[derive(serde::Serialize, serde::Deserialize)]
+pub struct PublishMessage {
+    pub peers: String,
+    pub content: String,
+}
+
+pub struct PeerInfo {
+    sender: UnboundedSender<Message>,
+    query: String,
+}
+type PeerMap = Arc<StdMutex<HashMap<SocketAddr, PeerInfo>>>;
 static SERVICE_CLIENT: tokio::sync::OnceCell<
     tokio::sync::Mutex<WebSocketStream<MaybeTlsStream<TcpStream>>>,
 > = OnceCell::const_new();
@@ -101,7 +143,13 @@ static PEER_MAP: OnceCell<PeerMap> = OnceCell::const_new();
 async fn accept_connection(peer_map: PeerMap, peer: SocketAddr, stream: TcpStream) {
     let ws_stream = accept_async(stream).await.expect("Failed to accept");
     let (tx, rx) = unbounded();
-    peer_map.lock().unwrap().insert(peer, tx);
+    peer_map.lock().unwrap().insert(
+        peer,
+        PeerInfo {
+            sender: tx,
+            query: String::new(),
+        },
+    );
 
     let is_service_peer = peer.ip().is_loopback();
 
@@ -110,19 +158,44 @@ async fn accept_connection(peer_map: PeerMap, peer: SocketAddr, stream: TcpStrea
     let broadcast_incoming = incoming.try_for_each(|msg| {
         if is_service_peer {
             tracing::info!("Received a message from {}: {}", peer, msg.to_text().unwrap());
+            let message: PublishMessage = match serde_json::from_str(msg.to_text().unwrap()) {
+                Ok(m) => m,
+                Err(e) => {
+                    tracing::warn!("Failed to parse message from service client: {}", e);
+                    return future::ok(());
+                }
+            };
             let peers = peer_map.lock().unwrap();
+            let recipents = if message.peers == "all" {
+                peers.values().collect::<Vec<&PeerInfo>>()
+            } else {
+                let target_peers: Vec<SocketAddr> =
+                    message.peers.split(',').filter_map(|s| s.parse().ok()).collect();
+                let mut filtered_peers: Vec<&PeerInfo> = Vec::new();
+                for addr in &target_peers {
+                    if let Some(peer_info) = peers.get(addr) {
+                        filtered_peers.push(peer_info);
+                    }
+                }
+                filtered_peers
+            };
 
-            // We want to broadcast the message to everyone except ourselves.
-            let broadcast_recipients = peers
-                .iter()
-                .filter(|(peer_addr, _)| peer_addr != &&peer)
-                .map(|(_, ws_sink)| ws_sink);
-
-            for recp in broadcast_recipients {
-                recp.unbounded_send(msg.clone()).unwrap();
+            for recp in recipents {
+                recp.sender.unbounded_send(msg.clone()).unwrap();
             }
-        } else {
-            tracing::info!("Ignoring message from non-service peer {}", peer);
+        } else if let Message::Text(text) = msg {
+            // Handle client filter subscription
+            let json_msg: Result<serde_json::Value, _> = serde_json::from_str(&text);
+            match json_msg {
+                Ok(_) => {
+                    if let Some(peer_info) = peer_map.lock().unwrap().get_mut(&peer) {
+                        peer_info.query = text.to_string();
+                    }
+                }
+                Err(_) => {
+                    tracing::warn!("Expected json object, got unknown text instead: {text}");
+                }
+            }
         }
 
         future::ok(())
@@ -156,6 +229,16 @@ impl WebSockets {
             tokio::spawn(accept_connection(Arc::clone(PEER_MAP.get().unwrap()), peer, stream));
         }
     }
+}
+
+async fn service_client() -> &'static Mutex<WebSocketStream<MaybeTlsStream<TcpStream>>> {
+    SERVICE_CLIENT
+        .get_or_init(|| async {
+            let addr = env::var("WEBSOCKETS_ADDR").unwrap_or_else(|_| DEF_HTTP_ADDR.into());
+            let (client, _) = connect_async(addr).await.unwrap();
+            tokio::sync::Mutex::new(client)
+        })
+        .await
 }
 
 async fn connect_service_client(
