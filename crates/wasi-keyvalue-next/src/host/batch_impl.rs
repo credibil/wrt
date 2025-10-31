d6f33285e4e0068d3bf3329b848426e9bd501b98
Andrew Weston <andrew.weston@propellerhead.co.nz>
2025-10-31 20:33:18 +1300

    Revert "converted to HostWithStore"
    This reverts commit ed234916da6efdc0363d690995a53cfeaedc8d18.

--- crates/wasi-keyvalue-next/src/host/batch_impl.rs
+++ crates/wasi-keyvalue-next/src/host/batch_impl.rs
@@ -1,9 +1,3 @@
-use crate::WasiKeyValueCtxView;
-use crate::host::Result;
-use crate::host::WasiKeyValue;
-use crate::host::generated::wasi::keyvalue::batch::{Host, HostWithStore};
-use crate::host::resource::BucketProxy;
-use crate::host::store_impl::get_bucket;
 use anyhow::anyhow;
 use wasmtime::component::{Accessor, Resource};
 
