69d794615910c25c6509ed3ee3bc7e916646e162
andrew-goldie <29654110+andrew-goldie@users.noreply.github.com>
2025-11-03 18:03:07 +1300

    Caching tidy-ups (#87)
    * add etag to response and implement forbidden headers
    
    * format
    
    * ttl type for fine grain control of cache lifetime
    
    * use ttl in cache control
    
    * fix bug in bad header filter for cached values
    
    * format

--- crates/wasi-http/src/guest.rs
+++ crates/wasi-http/src/guest.rs
@@ -7,6 +7,21 @@ mod incoming;
 mod outgoing;
 
 pub use axum;
+use http::header::{self, HeaderName};
 
 pub use self::incoming::*;
 pub use self::outgoing::*;
+
+/// Set of `[http::header::HeaderName]`, that are forbidden by default
+/// for requests and responses originating in the guest.
+pub const DEFAULT_FORBIDDEN_HEADERS: [HeaderName; 9] = [
+    header::CONNECTION,
+    HeaderName::from_static("keep-alive"),
+    header::PROXY_AUTHENTICATE,
+    header::PROXY_AUTHORIZATION,
+    HeaderName::from_static("proxy-connection"),
+    header::TRANSFER_ENCODING,
+    header::UPGRADE,
+    header::HOST,
+    HeaderName::from_static("http2-settings"),
+];
