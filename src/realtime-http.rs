6acf3a39c247fe0b0efcfb1ba60d3f9b873d364c
Andrew Weston <andrew.weston@propellerhead.co.nz>
2025-12-10 20:02:20 +1300

    fix deps

--- src/realtime-http.rs
+++ src/realtime-http.rs
@@ -2,25 +2,15 @@
 
 //! Realtime runtime CLI entry point.
 
-use anyhow::Result;
 use be_opentelemetry::Client as OpenTelemetry;
 use be_redis::Client as Redis;
-use kernel::{Cli, Command, Parser, tokio};
 use wasi_http::{WasiHttp, WasiHttpCtxImpl};
 use wasi_keyvalue::WasiKeyValue;
 use wasi_otel::WasiOtel;
 
 // Generate runtime
-buildgen::runtime!({
+buildgen::runtime!(main, {
     WasiHttp: WasiHttpCtxImpl,
     WasiOtel: OpenTelemetry,
     WasiKeyValue: Redis,
 });
-
-#[tokio::main]
-async fn main() -> Result<()> {
-    match Cli::parse().command {
-        Command::Run { wasm } => runtime::run(wasm).await,
-        _ => unreachable!(),
-    }
-}
