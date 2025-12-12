fd403e4413c52d152e4b5c5c032dae1860c47873
Andrew Weston <andrew.weston@propellerhead.co.nz>
2025-12-13 08:28:14 +1300

    fix linting issues in wasm32

--- examples/blobstore/runtime.rs
+++ examples/blobstore/runtime.rs
@@ -1,11 +1,15 @@
-#![cfg(not(target_arch = "wasm32"))]
+cfg_if::cfg_if! {
+    if #[cfg(not(target_arch = "wasm32"))] {
+        use wasi_blobstore::{WasiBlobstore, WasiBlobstoreCtxImpl as BlobstoreDefault};
+        use wasi_http::{WasiHttp, WasiHttpCtxImpl as HttpDefault};
+        use wasi_otel::{WasiOtel, WasiOtelCtxImpl as OtelDefault};
 
-use wasi_blobstore::{WasiBlobstore, WasiBlobstoreCtxImpl as BlobstoreDefault};
-use wasi_http::{WasiHttp, WasiHttpCtxImpl as HttpDefault};
-use wasi_otel::{WasiOtel, WasiOtelCtxImpl as OtelDefault};
-
-buildgen::runtime!(main, {
-    WasiHttp: HttpDefault,
-    WasiOtel: OtelDefault,
-    WasiBlobstore: BlobstoreDefault,
-});
+        buildgen::runtime!(main, {
+            WasiHttp: HttpDefault,
+            WasiOtel: OtelDefault,
+            WasiBlobstore: BlobstoreDefault,
+        });
+    } else {
+        pub fn main() {}
+    }
+}
--- examples/vault/runtime.rs
+++ examples/vault/runtime.rs
@@ -1,11 +1,15 @@
-#![cfg(not(target_arch = "wasm32"))]
+cfg_if::cfg_if! {
+    if #[cfg(not(target_arch = "wasm32"))] {
+        use wasi_http::{WasiHttp, WasiHttpCtxImpl as HttpDefault};
+        use wasi_otel::{WasiOtel, WasiOtelCtxImpl as OtelDefault};
+        use wasi_vault::{WasiVault, WasiVaultCtxImpl as VaultDefault};
 
-use wasi_http::{WasiHttp, WasiHttpCtxImpl as HttpDefault};
-use wasi_otel::{WasiOtel, WasiOtelCtxImpl as OtelDefault};
-use wasi_vault::{WasiVault, WasiVaultCtxImpl as VaultDefault};
-
-buildgen::runtime!(main, {
-    WasiHttp: HttpDefault,
-    WasiOtel: OtelDefault,
-    WasiVault: VaultDefault,
-});
+        buildgen::runtime!(main, {
+            WasiHttp: HttpDefault,
+            WasiOtel: OtelDefault,
+            WasiVault: VaultDefault,
+        });
+    } else {
+        pub fn main() {}
+    }
+}
