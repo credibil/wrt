980ec01aaed88b00fbf12dbd122c1b8a2cc37bf2
Karthik Swaminathan <karthik.swaminathan@propellerhead.co.nz>
2025-12-23 15:58:10 +1300

    Resolving CI errors

--- examples/sql-pg/runtime.rs
+++ examples/sql-pg/runtime.rs
@@ -1,7 +1,7 @@
 cfg_if::cfg_if! {
     if #[cfg(not(target_arch = "wasm32"))] {
-        use wasi_http::{WasiHttp, WasiHttpCtxImpl as HttpDefault};
-        use wasi_otel::{WasiOtel, WasiOtelCtxImpl as OtelDefault};
+        use wasi_http::{WasiHttp, HttpDefault};
+        use wasi_otel::{WasiOtel, OtelDefault};
         use wasi_sql::WasiSql;
         use be_postgres::Client as SqlBackend;
 
